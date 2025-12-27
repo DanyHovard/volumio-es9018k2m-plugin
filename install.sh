c0df3329146ddd06875c513ea0eb6dba4b8abc93
DanyHovard <win7viena@gmail.com>
2025-12-27 13:20:56 +0200

    Remove npm installation from install.sh
    Removed npm installation steps from the script.

--- install.sh
+++ install.sh
@@ -2,13 +2,4 @@
 
 echo "Installing ES9018K2M i2c Control Plugin"
 
-
-PLUGIN_DIR="$(dirname "$0")"
-cd "$PLUGIN_DIR"
-
-
-echo "Installing npm dependencies..."
-npm install
-
-
 echo "plugininstallend"
